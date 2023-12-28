de51bce7b75e1c7a5d04bc1277db7e4096539e20
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2023-12-28 19:58:01 +0100

    feat: adds generic api request

---
+++ LightPriceApp/LightPriceApp/Infrastructure/Network/Endpoints/ESIOSEndpoints.swift
@@ -0,0 +1,43 @@
+//
+//  ESIOSEndpoint.swift
+//  LightPriceApp
+//
+//  Created by José María Márquez Crespo on 28/12/23.
+//
+
+import Foundation
+
+enum ESIOSEndpoints {
+    case data(date: String)
+}
+
+extension ESIOSEndpoints: Endpoint {
+
+    var host: String {
+        return "api.esios.ree.es"
+    }
+
+    var path: String {
+        switch self {
+        case .data:
+            return "/archives/70/download_json"
+        }
+    }
+
+    var method: RequestMethod {
+        return .get
+    }
+
+    var headers: [String: String]? { return [:] }
+
+    var body: [String: String]? {
+        return nil
+    }
+
+    var queryParameters: [URLQueryItem]? {
+        switch self {
+        case .data(let date):
+            return [URLQueryItem(name: "date", value: date)]
+        }
+    }
+}
