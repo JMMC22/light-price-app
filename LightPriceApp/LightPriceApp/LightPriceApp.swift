ce3b676ede073a874f672714aa9d49cfdee9e10d
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2023-12-28 16:31:46 +0100

    chore: initial commit

---
+++ LightPriceApp/LightPriceApp/LightPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightPriceAppApp.swift
+//  LightPriceApp
+//
+//  Created by José María Márquez Crespo on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LightPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
