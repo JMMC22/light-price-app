003fc9cf735a963be8ea805d992ad3b75d95793f
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2023-12-29 12:59:53 +0100

    feat: adds strings catalog

--- LightPriceApp/LightPriceApp/Presentation/Components/PriceProgressBar/PriceProgressPanel/PriceProgressPanel.swift
+++ LightPriceApp/LightPriceApp/Presentation/Components/PriceProgressBar/PriceProgressPanel/PriceProgressPanel.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct PriceProgressPanel: View {
     var body: some View {
         VStack(spacing: 12) {
-            Text("PRECIO ACTUAL")
+            Text("current.price")
+                .textCase(.uppercase)
                 .LPFont(.Roboto(16, weight: .bold), color: .customBlack)
 
-            Text("0.181 €/kWh")
+            Text("currency.price \("0.181")")
                 .LPFont(.Roboto(48, weight: .blackItalic), color: .customBlack)
 
             PriceProgressBar(currentValue: 0.181, minValue: 0.131, maxValue: 0.256)
