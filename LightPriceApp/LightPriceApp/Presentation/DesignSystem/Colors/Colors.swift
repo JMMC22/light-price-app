5dd583a78205d0d7520a799d5470e4ad2208f0a6
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2023-12-29 09:51:26 +0100

    feat: adds colors extension and asset

---
+++ LightPriceApp/LightPriceApp/Presentation/DesignSystem/Colors/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  LightPriceApp
+//
+//  Created by José María Márquez Crespo on 29/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let primary = Color("Primary")
+    static let secondary = Color("Secondary")
+
+    static let customGreen = Color("Green")
+    static let customRed = Color("Red")
+    static let customOrange = Color("Orange")
+
+    static let customBlack = Color("Black")
+    static let customWhite = Color("White")
+}
