d91e650c8c1dbce6eb70f4feaa6d6de91e0f4997
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-11-11 14:43:25 +0100

    feat: add empty widget

---
+++ LightPriceApp/LightPriceWidget/LightPriceWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LightPriceWidgetBundle.swift
+//  LightPriceWidget
+//
+//  Created by José María Márquez Crespo on 11/11/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LightPriceWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LightPriceWidget()
+    }
+}
